018cbfa26e16dac9bede270297601aa1aab8cfca
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-07-07 19:40:26 +0900

    ADD(local test): local WebRTC

---
+++ client/src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Section from './Section'
+import Footer from './Footer'
+import Header from './Header'
+
+function Home() {
+    return (
+        <>
+            <Header/>
+            <Section/>
+            <Footer/>
+        </>
+    )
+}
+export default Home
