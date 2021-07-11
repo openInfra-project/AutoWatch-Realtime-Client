387f6f6e8e5ed210db8edbe4a66e93dc00a890d8
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-07-11 19:15:42 +0900

    ADD(Footer.js): icon  * react-icons  * -> WebRTC 마무리 후 작업

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,10 +1,25 @@
 import React from 'react'
-
-
+import { AiFillAudio,AiOutlineVideoCamera,AiOutlineFullscreen,AiOutlineUsergroupAdd} from "react-icons/ai";
+import './Footer.scss'
+import { BsChatSquareDots } from "react-icons/bs";
 function Footer() {
     return (
         <>
-           
+           <div className="footer">
+               <div className="menu">
+                   <p className="user_name">김준영</p>
+                    <div className="menu_icon">
+                        <AiFillAudio className="icon audio"/>
+                         <AiOutlineVideoCamera className="icon video" />
+                         <AiOutlineUsergroupAdd className="icon usergroupadd"/>
+                         <BsChatSquareDots className="icon chat"/>
+                         <AiOutlineFullscreen className="icon fullscreen"/>
+
+                         
+                    </div>
+                    
+               </div>
+           </div>
         </>
     )
 }
