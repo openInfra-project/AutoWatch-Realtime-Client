39cb5374c60162d5e4bda78b44b982584d461765
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-07-22 20:53:37 +0900

    Test(jun): 임시배포

--- client/src/component/Section.js
+++ client/src/component/Section.js
@@ -6,7 +6,7 @@ import {Grid} from 'semantic-ui-react'
 import useMedia from '../useMedia'
 import { ThemeConsumer } from 'styled-components'
 import { useDispatch, useSelector ,shallowEqual,} from 'react-redux'
-const SERVERPATH = "http://localhost:4000";
+const SERVERPATH = "https://ffb979d6b7e1.ngrok.io";
 const io = socket.connect(SERVERPATH);
 
 function Section() {
