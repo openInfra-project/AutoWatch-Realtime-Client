ee32ab05b0a374df536c1242851fbac0d252c84d
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-08-02 23:35:48 +0900

    add chat UI, socketIO

---
+++ client/src/component/ChatTemplate/Chat.js
@@ -0,0 +1,117 @@
+import React,{useEffect, useState} from 'react'
+import './Chat.scss'
+import ChatUser from '../ChatUserTemplate/ChatUser'
+import TextSizeAutoSize from 'react-textarea-autosize'
+import {AiOutlineArrowRight} from 'react-icons/ai'
+var chatdata= ""
+// function Chatinput() {
+//     const [chatinput,Setchatinput] = useState({
+//         input : ""
+//     })
+//     const {input} = chatinput
+//     const onChange = (e)=> {
+//         e.preventDefault()
+//         Setchatinput({
+//             input:e.target.value
+//         })
+//     }
+//     chatdata = input
+//     return (
+//         <>
+//             <input  
+//             onChange={e=>onChange(e)}
+//             className = "textarea_input" 
+//             value = {input}
+//             type ="text" 
+//             placeholder="메세지를 입력하세요" />
+//         </>
+//     )
+// }
+function Chat(props) {
+    const io = props.io
+    const {roomname,useremail,nickname,roomtype} = props.userdata
+    console.log("채팅 활성화 상태:"+props.setting)
+    const [chat,SetChat] = useState([])
+    useEffect(()=> {
+        io.on("message",data=> {
+            SetChat(oldValue => [...chat,data])
+            console.log("메세지 받음"+JSON.stringify(data))
+            // console.log("chat 상태:"+JSON.stringify(chat))
+        })
+    },[chat])
+   
+   
+
+
+
+
+    const submit = (e)=> {
+        e.preventDefault()
+        io.emit('message',{
+            nickname,
+            chatdata
+        })
+        document.getElementById('chatinput').value=""
+        return false;
+    }
+ 
+    const onChange = (e) => {
+        chatdata = e.target.value
+    }
+    const renderChat = ()=> {
+        return chat.map((data,index)=> (
+            <>
+                <div key = {index} className="message">
+                        <div className="messageinner">
+                            <p className="messageinnername">{data.nickname}</p>
+                            <p className="messageinnertime">오후 10:51</p>
+                        </div>
+                        <p className="ng-binding">
+                            {data.chatdata}
+                        </p>
+                  
+                </div>
+            </>
+        ))
+    }
+
+    return (
+        <>
+           
+            <div className="inbox" style={{animationName:props.setting.toString()||"false"}}>
+                <aside >
+                    <ul ng-controller="chatCtrl as chat" className="ng-scope">
+                        <ChatUser username="김준영" src="https://imgflip.com/s/meme/Futurama-Leela.jpg"/>
+                        <ChatUser username="황한식" src="https://imgflip.com/s/meme/Futurama-Leela.jpg"/>
+                        <ChatUser username="김유림" src="https://imgflip.com/s/meme/Futurama-Leela.jpg"/>
+                        <ChatUser username="김혜원" src="https://imgflip.com/s/meme/Futurama-Leela.jpg"/>
+                      
+                    </ul>
+                </aside>
+                
+                <main ng-controller="chatCtrl as chat" className="ng-scope">
+                    <div className="init">
+                        {renderChat()}
+                    </div>
+                    
+                    <footer>
+                        <form onSubmit={e=>submit(e)} className="ng-pristine ng-valid">
+                            {/* <ValueInput/> */}
+                            <input  
+                                id="chatinput"
+                                onChange={e=>onChange(e)}
+                                className = "textarea_input" 
+                                
+                                type ="text" 
+                                placeholder="메세지를 입력하세요" />
+                            <input type = "submit" value ="=>" className="submitIcon"/>
+                        </form>
+                    </footer>
+                </main>
+            </div>
+     
+        </>
+    )
+}
+
+export default Chat
--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,20 +1,34 @@
 import React, { useState } from 'react'
 import Section from './SectionTemplate/Section'
 import Header from './Header'
+import socket from 'socket.io-client'
 import './Home.scss'
 import {AiOutlineAudioMuted, AiOutlineAudio,AiOutlineVideoCamera,AiOutlineFullscreen,AiOutlineUsergroupAdd} from "react-icons/ai";
 import {BiVideoOff} from 'react-icons/bi'
 import { BsChatSquareDots } from "react-icons/bs";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {toggleVideoAudio} from '../store/action/index'
-
+import Chat from './ChatTemplate/Chat';
+// const SERVERPATH = "https://118.67.131.138:32218";
+// 테스트용 서버주소
+const SERVERPATH = "https://localhost:4000/";
+const io = socket.connect(SERVERPATH);
 function Home() {
     const dispatch = useDispatch()
+    //비디오 마이크 상태관리
     const [setting,Setsetting] = useState({
         video:true,
         audio:true
     })
-
+    // 그룹초대 채팅 전체화면 상태관리
+    const [othersetting,SetotherSetting] = useState({
+        group:false,
+        chat:false,
+        full:false
+    })
+    //roomname , username, nickname call
+    const userdata = useSelector(state=>state.getinform)
+    console.log("테스트용 userdata:"+JSON.stringify( userdata))
     function handlecreate(data) {
         console.log("홈 데이터"+JSON.stringify(data))
     }
@@ -42,7 +56,13 @@ function Home() {
         
     }
     const onClickChat=(e)=> {
+
         onClickChangeBackgroundColor(e)
+        SetotherSetting({
+            ...othersetting,
+            chat:!othersetting.chat
+        })
+
         
     }
     const onClickChangeBackgroundColor = (e)=> {
@@ -73,7 +93,8 @@ function Home() {
         <>
             <div className="HomeSection">
              
-                <Section setting = {setting}/>       
+                <Section setting = {setting} io = {io}/> 
+                <Chat  setting = {othersetting.chat} io = {io} userdata = {userdata}/>      
                 <div className="footer">
                     <div className="menu">
                         <p className="user_name">김준영</p>
